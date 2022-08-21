a846e09bfa31ea5e0378f7f3108aa797bf9f2644
Lukasz S <ukasz668@gmail.com>
2022-08-21 16:24:26 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("Snake Game 2022")
+
+
+snake = Turtle("turtle")
+
+
