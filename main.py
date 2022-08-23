7dbdf623e0003ddbc5b08898de0c28a3143a324a
Lukasz S <ukasz668@gmail.com>
2022-08-23 14:42:11 +0200

    finall

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import random
 
 screen = Screen()
 screen.setup(600, 600)
-screen.bgcolor("black")
+screen.bgcolor("blue")
 screen.title("Snake Game 2022")
 screen.tracer(0)
 
@@ -30,14 +30,23 @@ while game_is_on:
     snake.move()
 
     #Detecting collision with food
-    if snake.segments[0].distance(food) < 15:
+    if snake.segments[0].distance(food) < 10:
         food.new_local()
+        snake.extend()
         score.get_point()
 
+
     #Detecting collision with wall
-    if snake.segments[0].xcor() > 280 or snake.segments[0].xcor() < -280 or snake.segments[0].ycor() > 280 or snake.segments[0].xcor() < -280:
+    if snake.segments[0].xcor() > 280 or snake.segments[0].xcor() < -280 or snake.segments[0].ycor() > 280 or snake.segments[0].ycor() < -280:
         game_is_on = False
         score.game_over()
 
+    # Detecting collision with self
+    for segment in snake.segments:
+        if segment == snake.segments[0]:
+            pass
+        elif snake.segments[0].distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
 
 screen.exitonclick()
